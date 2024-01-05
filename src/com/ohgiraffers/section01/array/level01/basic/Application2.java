de67b55230b45d131602790a40adf3d2cdac4ed4
EUNJAE97 <154950170+EUNJAE97@users.noreply.github.com>
2024-01-05 18:06:09 +0900

    111

--- src/com/ohgiraffers/section01/array/level01/basic/Application2.java
+++ src/com/ohgiraffers/section01/array/level01/basic/Application2.java
@@ -1,8 +1,35 @@
 package com.ohgiraffers.section01.array.level01.basic;
 
+import java.util.Scanner;
+
 public class Application2 {
     public static void main(String[] args) {
-        String[] str = new String[5];
+        String[] fru = {"딸기","바나나","복숭아","키위","사과"};
+        String[] num = {"0","1","2","3","4"};
+        Scanner sc = new Scanner(System.in);
+        System.out.println("0부터 4까지의 정수를 입력하세요 : ");
+        int su = sc.nextInt();
+
+        switch (su) {
+            case 0 :
+                System.out.println("딸기");
+                break;
+            case 1 :
+                System.out.println("바나나");
+                break;
+            case 2 :
+                System.out.println("복숭아");
+                break;
+            case 3 :
+                System.out.println("키위");
+                break;
+            case 4 :
+                System.out.println("사과");
+                break;
+            default:
+                System.out.println("준비된 과일이 없습니다.");
+        }
+
 
     }
 }
---
+++ src/com/ohgiraffers/section01/array/level01/basic/Application3.java
@@ -0,0 +1,17 @@
+package com.ohgiraffers.section01.array.level01.basic;
+
+import java.util.Scanner;
+
+public class Application3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("문자열 하나를 입력하세요 : ");
+        String str = sc.nextLine();
+        char [] arr = str.toCharArray();
+        System.out.println("검색할 문자를 입력하세요 : ");
+        String serch = sc.nextLine();
+
+
+        }
+    }
+
